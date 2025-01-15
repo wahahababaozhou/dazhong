67802a25decbbd4b1988be0b4babbae0ab954ef7
yu102 <1025894148@qq.com>
2025-01-15 10:28:59 +0800

    初始化

--- gewechat.py
+++ gewechat.py
@@ -1,10 +1,14 @@
 from gewechat_client import GewechatClient
 
+import wechat
+
+
 def sendMsgToTeam(msg, title, desc, cardId):
     # 配置参数
     base_url = "http://localhost:2531/v2/api"
     token = "797fcae1307149d3bbe566fa7d162c85"
     app_id = "wx_UBkF_48rXEAAozpuU9JTk"
+    uuid = "oY3kAuyN8-Foyn5v5Ha7"
 
     # 创建 GewechatClient 实例
     client = GewechatClient(base_url, token)
@@ -25,10 +29,12 @@ def sendMsgToTeam(msg, title, desc, cardId):
                          "https://img0.baidu.com/it/u=3208604261,3520655236&fm=253&fmt=auto&app=138&f=JPEG?w=771&h=500")
         if send_msg_result.get('ret') != 200:
             print("发送消息失败:", send_msg_result)
+            wechat.sendtext(f"gewe发送微信群消息失败：{send_msg_result}")
             return
         print("发送消息成功:", send_msg_result)
     except Exception as e:
-        print("Failed to fetch contacts list:", str(e))
+        print("发送微信群消息失败:", str(e))
+        wechat.sendtext(f"gewe发送微信群消息失败：{str(e)}")
 
 
 if __name__ == "__main__":
